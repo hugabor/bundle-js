baed925c375e456b0596e7595ec35430be296c64
seltix5 <bgaspar5@hotmail.com>
2017-09-07 14:15:22 +0100

    update argv syntax

--- bin/bundle-js.js
+++ bin/bundle-js.js
@@ -7,13 +7,14 @@ let options = {}
 options.dir = process.cwd()
 
 for (let i = 2; i < process.argv.length; ++i) {
+    // LEGACY PARAM
     if (process.argv[i].indexOf('o=') == 0 || process.argv[i].indexOf('out=') == 0 || process.argv[i].indexOf('dest=') == 0) {
         options.dest = process.argv[i].split('=')[1]
     } else if (process.argv[i].indexOf('entry=') == 0) {
         options.entry = process.argv[i].split('=')[1]
     } else if (process.argv[i].indexOf('dir=') == 0) {
         options.dir = process.argv[i].split('=')[1]
-    } else if (process.argv[i].indexOf('dir-ignore-prefix=') == 0) {
+    } else if (process.argv[i].indexOf('ignore-fileprefix=') == 0) {
         options.dir_ignore_prefix = process.argv[i].split('=')[1]
     } else if (process.argv[i].indexOf('target=') == 0) {
         options.target = process.argv[i].split('=')[1]
@@ -21,6 +22,23 @@ for (let i = 2; i < process.argv.length; ++i) {
         options.exposed = [process.argv[i].split('=')[1]]
     } else if (process.argv[i].indexOf('export=') == 0) {
         options.export = process.argv[i].split('=')[1]
+    }
+    
+    
+    if (process.argv[i].indexOf('--output ') == 0) {
+        options.dest = process.argv[i].split('--output ')[1]
+    } else if (process.argv[i].indexOf('--entry ') == 0) {
+        options.entry = process.argv[i].split('--entry ')[1]
+    } else if (process.argv[i].indexOf('--dir ') == 0) {
+        options.dir = process.argv[i].split('--dir ')[1]
+    } else if (process.argv[i].indexOf('--ignore-fileprefix ') == 0) {
+        options.dir_ignore_prefix = process.argv[i].split('--ignore-fileprefix ')[1]
+    } else if (process.argv[i].indexOf('--target ') == 0) {
+        options.target = process.argv[i].split('--target ')[1]
+    } else if (process.argv[i].indexOf('--exposed ') == 0) {
+        options.exposed = [process.argv[i].split('--exposed ')[1]]
+    } else if (process.argv[i].indexOf('--export ') == 0) {
+        options.export = process.argv[i].split('--export ')[1]
     } else if (process.argv[i].indexOf('--ignore-sort') == 0) {
         options.ignore_sort = true
     } else if (process.argv[i].indexOf('--iife') == 0) {
